4b11c3225e3d0dde312e9a5d7e5c937e6eb7cefd
Erskari <38257667+Erskari@users.noreply.github.com>
2023-01-19 11:36:47 +0200

    Add files via upload

---
+++ lvl4.py
@@ -0,0 +1,123 @@
+from dotenv import dotenv_values
+import requests
+import webbrowser
+import websocket
+import json
+from lib.math import normalize_heading
+import time
+import math
+
+FRONTEND_BASE = "noflight.monad.fi"
+BACKEND_BASE = "noflight.monad.fi/backend"
+
+game_id = None
+
+def on_message(ws: websocket.WebSocketApp, message):
+    [action, payload] = json.loads(message)
+
+    if action != "game-instance":
+        print([action, payload])
+        return
+     # New game tick arrived!
+    game_state = json.loads(payload["gameState"])
+    commands = generate_commands(game_state)
+
+    time.sleep(0.1)
+    ws.send(json.dumps(["run-command", {"gameId": game_id, "payload": commands}]))
+
+
+def on_error(ws: websocket.WebSocketApp, error):
+    print(error)
+
+
+def on_open(ws: websocket.WebSocketApp):
+    print("OPENED")
+    ws.send(json.dumps(["sub-game", {"id": game_id}]))
+
+
+def on_close(ws, close_status_code, close_msg):
+    print("CLOSED")
+
+# Change this to your own implementation
+def generate_commands(game_state):
+    commands = []
+    aircraft = game_state['aircrafts']
+
+    # Lisää pelissä kuluneiden tickien määrää yhdellä aina jos edes yksi kone on olemassa
+    game_tick_count.append(aircraft[0]['id'])
+
+    if len(game_tick_count) == 1:
+        # Pythagoraan lauseellaa saadaan koneille lähestymiskulmat
+        new_dir = normalize_heading(aircraft[0]['direction'] + 20)
+        commands.append(f"HEAD {aircraft[0]['id']} {new_dir}")
+        new_dir = normalize_heading(aircraft[1]['direction'] + 15)
+        commands.append(f"HEAD {aircraft[1]['id']} {new_dir}")
+        return commands
+
+    # Loput if lauseista kääntää koneita 90° - lähestymiskulma mahdollisimman nopeasti
+    elif len(game_tick_count) == 37:
+        new_dir = normalize_heading(aircraft[0]['direction'] + 20)
+        commands.append(f"HEAD {aircraft[0]['id']} {new_dir}")
+        return commands
+    elif len(game_tick_count) == 38:
+        new_dir = normalize_heading(aircraft[0]['direction'] + 20)
+        commands.append(f"HEAD {aircraft[0]['id']} {new_dir}")
+        return commands
+    elif len(game_tick_count) == 39:
+        new_dir = normalize_heading(aircraft[0]['direction'] + 20)
+        commands.append(f"HEAD {aircraft[0]['id']} {new_dir}")
+        return commands
+    elif len(game_tick_count) == 40:
+        new_dir = normalize_heading(aircraft[0]['direction'] + 10)
+        commands.append(f"HEAD {aircraft[0]['id']} {new_dir}")
+        return commands
+    elif len(game_tick_count) == 47:
+        new_dir = normalize_heading(aircraft[0]['direction'] + 20)
+        commands.append(f"HEAD {aircraft[0]['id']} {new_dir}")
+        return commands
+    elif len(game_tick_count) == 48:
+        new_dir = normalize_heading(aircraft[0]['direction'] + 20)
+        commands.append(f"HEAD {aircraft[0]['id']} {new_dir}")
+        return commands
+    elif len(game_tick_count) == 49:
+        new_dir = normalize_heading(aircraft[0]['direction'] + 20)
+        commands.append(f"HEAD {aircraft[0]['id']} {new_dir}")
+        return commands
+    elif len(game_tick_count) == 50:
+        new_dir = normalize_heading(aircraft[0]['direction'] + 15)
+        commands.append(f"HEAD {aircraft[0]['id']} {new_dir}")
+        return commands
+    else:
+        return commands
+
+def main():
+    config = dotenv_values()
+    res = requests.post(
+        f"https://{BACKEND_BASE}/api/levels/{config['LEVEL_ID']}",
+        headers={
+            "Authorization": config["TOKEN"]
+        })
+
+    if not res.ok:
+        print(f"Couldn't create game: {res.status_code} - {res.text}")
+        return
+
+    game_instance = res.json()
+    global game_id
+    # generate_commands funktiossa käytettävän game_tick_count muuttujan alustus
+    global game_tick_count
+    game_tick_count = []
+    game_id = game_instance["entityId"]
+
+    url = f"https://{FRONTEND_BASE}/?id={game_id}"
+    print(f"Game at {url}")
+    webbrowser.open(url, new=2)
+    time.sleep(2)
+
+    ws = websocket.WebSocketApp(
+        f"wss://{BACKEND_BASE}/{config['TOKEN']}/", on_message=on_message, on_open=on_open, on_close=on_close, on_error=on_error)
+    ws.run_forever()
+
+
+if __name__ == "__main__":
+    main()
